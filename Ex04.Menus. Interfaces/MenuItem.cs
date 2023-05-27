dacaa34046a2b88df19967fc9438226f484e3d7f
Ronen Gelmanovich <ronenge@mta.ac.il>
2023-05-27 13:22:21 +0300

    Adjusted namespace

--- Ex04.Menus. Interfaces/MenuItem.cs
+++ Ex04.Menus. Interfaces/MenuItem.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace B23_Ex03_Ronen_319047718_Ido_315942193
+namespace B23_Ex04_Ronen_319047718_Ido_315942193
 {
     public class MenuItem
     {
