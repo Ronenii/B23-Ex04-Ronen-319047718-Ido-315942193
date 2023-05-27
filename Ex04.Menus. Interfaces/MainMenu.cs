81d21ba587384adc898336baa82d25cdc6645540
Ronen Gelmanovich <ronenge@mta.ac.il>
2023-05-27 13:01:43 +0300

    Changed namespace

--- Ex04.Menus. Interfaces/MainMenu.cs
+++ Ex04.Menus. Interfaces/MainMenu.cs
@@ -1,9 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
-namespace B23_Ex03_Ronen_319047718_Ido_315942193
+namespace B23_Ex04_Ronen_319047718_Ido_315942193
 {
     public class MainMenu
     {
