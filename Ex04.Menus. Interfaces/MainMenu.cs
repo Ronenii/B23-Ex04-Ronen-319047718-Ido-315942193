ff838ddd5762fe0302294b0894e61d15f45d83b7
Ronen Gelmanovich <ronenge@mta.ac.il>
2023-05-27 13:21:49 +0300

    Step 2

--- Ex04.Menus. Interfaces/MainMenu.cs
+++ Ex04.Menus. Interfaces/MainMenu.cs
@@ -1,8 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace B23_Ex04_Ronen_319047718_Ido_315942193
 {
     public class MainMenu
     {
+        private List<MenuItem> m_MenuItems;
     }
 }
